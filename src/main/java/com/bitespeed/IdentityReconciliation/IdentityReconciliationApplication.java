5404d8f6bdfa2867a17f6cfe9def36f7faefc7b7
thimank <himank.tiwari@yahoo.com>
2024-02-19 21:08:37 +0530

    initial setup

---
+++ src/main/java/com/bitespeed/IdentityReconciliation/IdentityReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed.IdentityReconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityReconciliationApplication.class, args);
+	}
+
+}
